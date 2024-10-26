91794f616c0060b855858f1c96f8c11dbacadfaa
viharshah1 <Shahvihar67@gmail.com>
2024-10-26 11:02:45 +0530

    Add files via upload

---
+++ EG 4.py
@@ -0,0 +1,8 @@
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+temp=x
+print("the value of temp variable is",temp)
+x=y
+print("the value of x is ",x)
+y=temp
+print("the value of y is ",y)
