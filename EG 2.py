4c362257218ce6c586166433836eb004ea9a31de
viharshah1 <Shahvihar67@gmail.com>
2024-10-26 10:57:21 +0530

    Add files via upload

---
+++ EG 2.py
@@ -0,0 +1,9 @@
+principal=int(input("enter the value of principal:="))
+roi=int(input("enter the value of roi:="))
+t=int(input("enter the value of t:="))
+ans=(principal*roi*t)/100
+print("simpleinterst of ",principal,"and",roi,"and",t,"is:=",ans)
+r=int(input("enter the value of r:="))
+pi=3.14
+ans=(pi*r*r)
+print("area of circle of pi",pi,"and r",r,"is:=",ans)
