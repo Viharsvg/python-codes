858fb8f43e52a49bb9ec8039233ce133c06a45bc
viharshah1 <Shahvihar67@gmail.com>
2024-10-26 11:08:41 +0530

    Add files via upload

---
+++ EG 14 main code and function for add &min.py
@@ -0,0 +1,26 @@
+#funnction
+def line():
+    print("="*129)
+def star():
+    print("*"*129)
+def add():
+    a=int(input("enter the value of a:="))
+    b=int(input("enter the value of b:="))
+    ans=a+b
+    print("addition of ",a," and ",b," is:=",ans)
+def sub():
+    a1=int(input("enter the value of a1:="))
+    b1=int(input("enter the value of b1:="))
+    ans1=a1-b1
+    print("subtraction of ",a1," and ",b1," is:=",ans1)
+#main code
+print("WELCOME TO THE AREA OF PROGRAMM \n THIS WILL SHOW YOU SUM OF TWO VALUES")
+add()
+print("WELCOME TO THE AREA OF PROGRAMM \n THIS WILL SHOW YOU SUB OF TWO VALUES")
+line()
+star()
+sub()
+line()
+star()
+    
+    
