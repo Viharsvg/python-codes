18ce9664a743d4744028c6b065dc2ce662029a52
viharshah1 <Shahvihar67@gmail.com>
2024-10-26 11:07:18 +0530

    Add files via upload

---
+++ EG 11.py
@@ -0,0 +1,7 @@
+no=int(input("enter the value of no:="))
+if(no%5==0):
+    no=no*2
+    print("the no. is divisible by 5,mulitiplied by 2",no)
+else:
+    no=no+100
+    print("no. is not div by 5,100 add",no)
