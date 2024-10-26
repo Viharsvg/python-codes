f10a98ac5aae286096c7f41fc054dff7f2c4e9d1
viharshah1 <Shahvihar67@gmail.com>
2024-10-26 10:52:18 +0530

    Add files via upload

---
+++ EG 1.py
@@ -0,0 +1,16 @@
+a=int(input("enter the value of a:="))
+b=int(input("enter the value of b:="))
+ans=a+b
+print("addition of ",a," and ",b," is:=",ans)
+ans1=a-b
+print("subtraction of ",a," and ",b," is:=",ans1)
+ans2=a*b
+print("multiplication of",a,"and ",b,"is:=",ans2)
+ans3=a/b
+print("division of ",a,"and ",b,"is:=",ans3)
+name=int(input("enter your name "))
+rollno=int(input("enter your roll no"))
+address=int(input("enter your address"))
+school=int(input("enter your school name"))
+workjob=int(input("enter your work job"))
+no=
